de4e8d550b2e870d42ea30315b94e51f77465d64
Arthurvsp <arthurvspinheiro@gmail.com>
2025-12-08 20:40:18 -0300

    feat(): ajuste para nao dar problema de cors no back

--- seat-management-api/src/main/java/com/example/seat_management_api/controller/PoltronaController.java
+++ seat-management-api/src/main/java/com/example/seat_management_api/controller/PoltronaController.java
@@ -11,7 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/poltronas")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin
 public class PoltronaController {
 
     private final PoltronaService poltronaService;
